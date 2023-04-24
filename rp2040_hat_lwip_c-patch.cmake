b9e7854799bdfc89d8d4ae48d69b1252d335e313
Louiswiz <75520034+min-hs@users.noreply.github.com>
2023-04-24 17:50:14 +0900

    change pico-sdk 1.5.0

--- rp2040_hat_lwip_c-patch.cmake
+++ rp2040_hat_lwip_c-patch.cmake
@@ -12,10 +12,9 @@ message("RP2040-HAT-LWIP-C patch utils found")
 
 set(RP2040_HAT_LWIP_C_SRC_DIR "${CMAKE_CURRENT_SOURCE_DIR}")
 set(IOLIBRARY_DRIVER_SRC_DIR "${RP2040_HAT_LWIP_C_SRC_DIR}/libraries/ioLibrary_Driver")
-set(PICO_EXTRAS_SRC_DIR "${RP2040_HAT_LWIP_C_SRC_DIR}/libraries/pico-extras")
-set(PICO_EXTRAS_LWIP_SRC_DIR "${RP2040_HAT_LWIP_C_SRC_DIR}/libraries/pico-extras/lib/lwip")
 set(PICO_SDK_SRC_DIR "${RP2040_HAT_LWIP_C_SRC_DIR}/libraries/pico-sdk")
-set(PICO_SDK_TINYUSB_SRC_DIR "${RP2040_HAT_LWIP_C_SRC_DIR}/libraries/lib/tinyusb")
+set(PICO_SDK_LWIP_SRC_DIR "${PICO_SDK_SRC_DIR}/lib/lwip")
+set(PICO_SDK_TINYUSB_SRC_DIR "${PICO_SDK_SRC_DIR}/lib/tinyusb")
 set(RP2040_HAT_C_PATCH_DIR "${RP2040_HAT_LWIP_C_SRC_DIR}/patches")
 
 # Delete untracked files in ioLibrary_Driver
@@ -24,14 +23,6 @@ if(EXISTS "${IOLIBRARY_DRIVER_SRC_DIR}/.git")
 	execute_process(COMMAND ${GIT_EXECUTABLE} -C ${IOLIBRARY_DRIVER_SRC_DIR} clean -fdx)
 	execute_process(COMMAND ${GIT_EXECUTABLE} -C ${IOLIBRARY_DRIVER_SRC_DIR} reset --hard)
 	message("ioLibrary_Driver cleaned")
-endif()
-
-# Delete untracked files in pico-extras
-if(EXISTS "${PICO_EXTRAS_SRC_DIR}/.git")
-	message("cleaning pico-extras...")
-	execute_process(COMMAND ${GIT_EXECUTABLE} -C ${PICO_EXTRAS_SRC_DIR} clean -fdx)
-	execute_process(COMMAND ${GIT_EXECUTABLE} -C ${PICO_EXTRAS_SRC_DIR} reset --hard)
-	message("pico-extras cleaned")
 endif()
 
 # Delete untracked files in pico-sdk
@@ -44,15 +35,13 @@ endif()
 
 execute_process(COMMAND ${GIT_EXECUTABLE} -C ${RP2040_HAT_LWIP_C_SRC_DIR} submodule update --init)
 
-# Delete untracked files in pico-extras lwip
-if(EXISTS "${PICO_EXTRAS_LWIP_SRC_DIR}/.git")
-	message("cleaning pico-extras lwip...")
-	execute_process(COMMAND ${GIT_EXECUTABLE} -C ${PICO_EXTRAS_LWIP_SRC_DIR} clean -fdx)
-	execute_process(COMMAND ${GIT_EXECUTABLE} -C ${PICO_EXTRAS_LWIP_SRC_DIR} reset --hard)
-	message("pico-extras lwip cleaned")
+# Delete untracked files in lwip
+if(EXISTS "${PICO_SDK_LWIP_SRC_DIR}/.git")
+	message("cleaning pico-sdk lwip...")
+	execute_process(COMMAND ${GIT_EXECUTABLE} -C ${PICO_SDK_LWIP_SRC_DIR} clean -fdx)
+	execute_process(COMMAND ${GIT_EXECUTABLE} -C ${PICO_SDK_LWIP_SRC_DIR} reset --hard)
+	message("pico-sdk lwip cleaned")
 endif()
-
-execute_process(COMMAND ${GIT_EXECUTABLE} -C ${PICO_EXTRAS_SRC_DIR} submodule update --init)
 
 # Delete untracked files in tinyusb
 if(EXISTS "${PICO_SDK_TINYUSB_SRC_DIR}/.git")
@@ -63,34 +52,3 @@ if(EXISTS "${PICO_SDK_TINYUSB_SRC_DIR}/.git")
 endif()
 
 execute_process(COMMAND ${GIT_EXECUTABLE} -C ${PICO_SDK_SRC_DIR} submodule update --init)
-
-# pico-extras patch
-message("submodules pico-extras initialised")
-
-file(GLOB PICO_EXTRAS_PATCHES 
-	"${RP2040_HAT_C_PATCH_DIR}/01_pico-extras_lwip_cmakelists.patch" 
-	)
-
-foreach(PICO_EXTRAS_PATCH IN LISTS PICO_EXTRAS_PATCHES)
-	message("Running patch ${PICO_EXTRAS_PATCH}")
-	execute_process(
-		COMMAND ${GIT_EXECUTABLE} apply --ignore-whitespace ${PICO_EXTRAS_PATCH}
-		WORKING_DIRECTORY ${PICO_EXTRAS_SRC_DIR}
-	)
-endforeach()
-
-# pico-extras lwip patch
-message("submodules pico-extras lwip initialised")
-
-file(GLOB PICO_EXTRAS_LWIP_PATCHES 	
-	"${RP2040_HAT_C_PATCH_DIR}/02_pico-extras_lwip_dns.patch"	
-	"${RP2040_HAT_C_PATCH_DIR}/03_pico-extras_lwip_opt.patch"
-	)
-
-foreach(PICO_EXTRAS_LWIP_PATCH IN LISTS PICO_EXTRAS_LWIP_PATCHES)
-	message("Running patch ${PICO_EXTRAS_LWIP_PATCH}")
-	execute_process(
-		COMMAND ${GIT_EXECUTABLE} apply --ignore-whitespace ${PICO_EXTRAS_LWIP_PATCH}				
-		WORKING_DIRECTORY ${PICO_EXTRAS_LWIP_SRC_DIR}
-	)
-endforeach()
